72861f20d9904f0cdcda40be17ec67deb0ab0267
Andrew LaCharite <adlacharite@gmail.com>
2022-07-19 11:33:08 -0700

    split code into components

---
+++ src/routes/Code.svelte
@@ -0,0 +1,111 @@
+<script>
+	import './global.css';
+</script>
+
+<div class="code">
+	<div class="textbox">
+		<h1>CODE</h1>
+	</div>
+	<div class="horizon-blur" />
+	<svg>
+		<line class="horizontal" x1="0" y1="0%" x2="100%" y2="0%" />
+		<line class="horizontal" x1="0" y1="0%" x2="100%" y2="0%" />
+		<line class="horizontal" x1="0" y1="0%" x2="100%" y2="0%" />
+		<line class="horizontal" x1="0" y1="0%" x2="100%" y2="0%" />
+		<line class="horizontal" x1="0" y1="0%" x2="100%" y2="0%" />
+		<line class="horizontal" x1="0" y1="0%" x2="100%" y2="0%" />
+		<line class="radial" x1="50%" y1="-3%" x2="0" y2="3%" />
+		<line class="radial" x1="50%" y1="-3%" x2="0" y2="6%" />
+		<line class="radial" x1="50%" y1="-3%" x2="0" y2="10%" />
+		<line class="radial" x1="50%" y1="-3%" x2="0" y2="17%" />
+		<line class="radial" x1="50%" y1="-3%" x2="0" y2="25%" />
+		<line class="radial" x1="50%" y1="-3%" x2="0" y2="35%" />
+		<line class="radial" x1="50%" y1="-3%" x2="0" y2="58%" />
+		<line class="radial" x1="50%" y1="-3%" x2="0" y2="100%" />
+		<line class="radial" x1="50%" y1="-3%" x2="0" y2="290%" />
+		<line class="radial" x1="50%" y1="-3%" x2="100%" y2="3%" />
+		<line class="radial" x1="50%" y1="-3%" x2="100%" y2="6%" />
+		<line class="radial" x1="50%" y1="-3%" x2="100%" y2="10%" />
+		<line class="radial" x1="50%" y1="-3%" x2="100%" y2="17%" />
+		<line class="radial" x1="50%" y1="-3%" x2="100%" y2="25%" />
+		<line class="radial" x1="50%" y1="-3%" x2="100%" y2="35%" />
+		<line class="radial" x1="50%" y1="-3%" x2="100%" y2="58%" />
+		<line class="radial" x1="50%" y1="-3%" x2="100%" y2="100%" />
+		<line class="radial" x1="50%" y1="-3%" x2="100%" y2="290%" />
+	</svg>
+</div>
+
+<style>
+	:root {
+		--gradient-color1: rgb(44, 70, 68);
+		--half-gradient-color1: rgba(44, 70, 68, 0.5);
+		--mid-color1-color2: rgb(24, 48, 42);
+		--gradient-color2: rgb(4, 26, 16);
+		--half-gradient-color2: rgba(4, 26, 16, 0.5);
+	}
+	.code {
+		width: 100vw;
+		height: 50vh;
+		position: relative;
+		background-color: var(--gradient-color2);
+	}
+	.code .textbox {
+		top: 20%;
+		right: 10%;
+		z-index: 1;
+		position: absolute;
+	}
+	.code .horizon-blur {
+		height: 15%;
+		width: 100%;
+		position: absolute;
+		top: 45%;
+		background: linear-gradient(to top, green, rgb(0, 65, 0) 10%, var(--gradient-color2));
+	}
+	.code h1 {
+		color: rgb(67, 226, 19);
+		text-shadow: 0 0 30px rgb(51, 172, 15);
+	}
+	svg {
+		width: 100%;
+		height: 40%;
+		position: absolute;
+		top: 60%;
+	}
+	line {
+		stroke: green;
+	}
+	.horizontal:first-of-type {
+		animation-name: code-grid;
+		animation-duration: 2s;
+		animation-iteration-count: infinite;
+		animation-timing-function: cubic-bezier(1, 0, 1, 1);
+	}
+	.horizontal:nth-of-type(2) {
+		animation-name: code-grid;
+		animation-duration: 2s;
+		animation-iteration-count: infinite;
+		animation-timing-function: cubic-bezier(1, 0, 1, 1);
+		animation-delay: 0.5s;
+	}
+	.horizontal:nth-of-type(3) {
+		animation-name: code-grid;
+		animation-duration: 2s;
+		animation-iteration-count: infinite;
+		animation-timing-function: cubic-bezier(1, 0, 1, 1);
+		animation-delay: 1s;
+	}
+	.horizontal:nth-of-type(4) {
+		animation-name: code-grid;
+		animation-duration: 2s;
+		animation-iteration-count: infinite;
+		animation-timing-function: cubic-bezier(1, 0, 1, 1);
+		animation-delay: 1.5s;
+	}
+
+	@keyframes code-grid {
+		to {
+			transform: translateY(100%);
+		}
+	}
+</style>
---
+++ src/routes/Sail.svelte
@@ -0,0 +1,29 @@
+<script>
+	import './global.css';
+</script>
+
+<div class="sail">
+	<div class="textbox">
+		<h1>SAIL</h1>
+	</div>
+</div>
+
+<style>
+	.sail {
+		width: 100vw;
+		height: 50vh;
+		position: relative;
+		background-image: url('./IMG_4543.JPG');
+		background-position: center;
+		background-size: cover;
+	}
+	.sail .textbox {
+		bottom: 20%;
+		left: 10%;
+		position: absolute;
+	}
+	.sail h1 {
+		color: rgb(70, 113, 207);
+		text-shadow: 0 0 30px white;
+	}
+</style>
