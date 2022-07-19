8c1b7373a6b7abfefd4fd87b3546512441a772a9
Andrew LaCharite <adlacharite@gmail.com>
2022-07-19 12:41:37 -0700

    trying for github pages build

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs"
+		}),
+		prerender: {default: true},
 	}
 };
 
