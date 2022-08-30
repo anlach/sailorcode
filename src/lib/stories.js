6c69da6bdfe24c5e165073cea14801723b73a2a7
Andrew LaCharite <adlacharite@gmail.com>
2022-08-30 10:59:22 -0700

    add a bunch of new stories and pictures

--- src/lib/stories.js
+++ src/lib/stories.js
@@ -43,7 +43,8 @@ export default [
                you have. There was a lot of sanding involved and it probably \
                took me over 200 hours to complete. I started out thinking this \
                would be on a Cape Dory 28, but it ended up fitting alright on the \
-               Shannon 28 between the companionway and the mast."
+               Shannon 28 between the companionway and the mast. The name is \
+               a Dwarf Fortress reference."
     },
     {
         title: "Sailing Urist",
@@ -52,12 +53,25 @@ export default [
         photos: [],
         alt: [],
         video: "oyqBjDpv3eU",
-        text: "Urist McDinghy is a CLC Eastport Nesting pram. \
-        This was maybe the second or third time I sailed it after \
-        building the custom sprit rig."
+        text: "This was maybe the second or third time I sailed it after \
+               building the custom sprit rig."
     },
     {
-        title: "Vinalhaven",
+        title: "Rockland, Maine",
+        latLng: [44.10632108684309, -69.02477918846719],
+        date: new Date("28 August, 2021"),
+        photos: [
+            "/img/stories/rockland-1.jpg",
+        ],
+        alt: ["Schooner"],
+        video: null,
+        text: "I was sailing a little faster than this schooner \
+               on my way to Vinalhaven when I got this shot. I think \
+               they were purposefully over-sheeted to give the passengers \
+               a bit more of the heeled-over experience."
+    },
+    {
+        title: "Vinalhaven, Maine",
         latLng: [44.08919861878901, -68.88378227326321],
         date: new Date("25 October, 2021"),
         photos: ["/img/stories/vinalhaven-sunset.jpg",
@@ -72,13 +86,108 @@ export default [
            Going against that was very uncomfortable, so we didn't make it far."
     },
     {
+        title: "Rockport Harbor, Massachusetts",
+        latLng: [42.6589350318447, -70.61421773240487],
+        date: new Date("10 November, 2021"),
+        photos: [
+            "/img/stories/rockport-1.jpg",
+            "/img/stories/rockport-2.jpg",
+        ],
+        alt: [
+            "Rockport Harbor",
+            "Rockport Harbor",
+        ],
+        video: null,
+        text: "We stopped in Rockport Harbor overnight after a day of motoring in calm \
+               swells. The dock was free, and although \
+               we didn't bother to unship the dinghy to go to shore, it was a really \
+               neat stop. The pictures don't do it justice, but you're really closed in \
+               by stone walls on all sides, with just a narrow little entrance channel."
+    },
+    {
+        title: "Stamford, Connecticut",
+        latLng: [
+            41.03839288001135, -73.54480907897373
+        ],
+        date: new Date("19 November, 2021"),
+        photos: [
+            "/img/stories/stamford-1.jpg",
+            "/img/stories/stamford-2.jpg",
+        ],
+        alt: [
+            "Stamford"
+        ],
+        video: null,
+        text: "We made a long overnight passage from Newport, RI to Stamford, CT. \
+               The current through The Race was perfect, and we were going over eight \
+               knots for hours. But then as we were approaching the western end of \
+               Long Island Sound the wind turned against us and it was a long morning \
+               and afternoon coming in. We left for NYC the next day in what turned out to be \
+               a bad decision - very strong winds and waves coming into the cockpit on \
+               occasion. One really soaked Morgan and it was something like 40 degrees \
+               Fahrenheit out. It was probably the worst she had it on the trip, but there \
+               was a lot of really bad conditions we were heading south in. Late November is \
+               rough."
+    },
+    {
+        title: "New York City",
+        latLng: [40.70317005117716, -74.03857043981465],
+        date: new Date("20 November, 2021"),
+        photos: [
+            "/img/stories/liberty-0.jpg",
+            "/img/stories/liberty-1.jpg",
+            "/img/stories/liberty-2.jpg",
+            "/img/stories/liberty-3.jpg",
+            "/img/stories/nyc-1.jpg",
+            "/img/stories/nyc-2.jpg",
+        ],
+        alt: [
+            "Manhatten",
+            "Manhatten",
+            "Manhatten",
+            "Manhatten",
+            "Morgan eating a chili dog",
+            "Morgan on boat",
+        ],
+        video: null,
+        text: "We finally arrived in NYC after a very difficult sail up the western \
+               Long Island Sound. We anchored near the Statue of Liberty and \
+               Ellis Island for one night and then moved to a marina in Hoboken, NJ \
+               in order to take a break, see the city, and wait for a good weather \
+               window to continue south."
+    },
+    {
+        title: "Thanksgiving",
+        latLng: [39.38217218556049, -74.42256187662677],
+        date: new Date("25 November, 2021"),
+        photos: [
+            "/img/stories/a-city-1.jpg",
+            "/img/stories/a-city-2.jpg",
+        ],
+        alt: [],
+        video: null,
+        text: "We stopped in Atlantic City to wait out a gale. I managed \
+               to run aground here, but the tide was rising, so we motored \
+               off and anchored once the current had eased a bit. We managed \
+               a pretty decent Thankgiving meal, all things considered. \
+               The next day was super gusty, though, and the boat was \
+               swinging wildly out of the opposing current. After getting us \
+               smoked out of the cabin a few times I finally gave up on using \
+               the wood stove, because the wind direction was too unpredictable. \
+               During one moment, the flow reversed so suddenly that flames blasted \
+               over a foot out of the stove vents back into the cabin. That was \
+               the end of that. Most other experiences with the stove have been good."
+    },
+    {
         title: "Christmas Lights",
         latLng: [37.024078718790186, -76.34191804918115],
         date: new Date("6 December, 2021"),
         photos: ["/img/stories/christmas.jpg",
+            "/img/stories/hampton-1.jpg",
             "/img/stories/hampton-morgan.jpg"],
         alt: ["Christmas Lights",
-            "Fort"],
+            "Andrew and Morgan",
+            "Morgan in fort"],
         video: null,
         text: "We added some decorations for the holidays while in Hampton, VA. \
             Somerledi was here a month at a very good price. This was \
