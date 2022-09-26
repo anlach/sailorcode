1289d5e9ee3403879a5ced713d2878874753d3f8
Andrew LaCharite <adlacharite@gmail.com>
2022-09-26 09:53:33 -0700

    refactor out CodeLinks for use in noscript

--- src/lib/Code.svelte
+++ src/lib/Code.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Gridscape from './Gridscape.svelte';
 	import Project from './Project.svelte';
+	import CodeLinks from './CodeLinks.svelte';
 	import { blur, scale, fly } from 'svelte/transition';
 	import { cubicInOut } from 'svelte/easing';
 	import { onMount, onDestroy } from 'svelte';
@@ -195,41 +196,11 @@
 				{/each}
 			</div>
 		</div>
-		<div class="links" transition:blur>
-			<a
-				href="https://www.freecodecamp.org/anlach"
-				target="_blank"
-				on:click|stopPropagation
-			>
-				<span
-					class="fa-brands fa-free-code-camp"
-					alt="freeCodeCamp Certifications"
-					title="freeCodeCamp Certifications"
-				/>
-			</a>
-			<a
-				href="https://www.github.com/anlach"
-				target="_blank"
-				on:click|stopPropagation
-			>
-				<span class="fa-brands fa-github" alt="Github" title="Github" />
-			</a>
-			<a
-				href="https://codepen.io/anlach"
-				target="_blank"
-				on:click|stopPropagation
-			>
-				<span class="fa-brands fa-codepen" alt="Codepen" title="Codepen" />
-			</a>
-			<a
-				href="https://www.linkedin.com/in/adlacharite/"
-				target="_blank"
-				on:click|stopPropagation
-			>
-				<span class="fa-brands fa-linkedin" alt="LinkedIn" title="LinkedIn" />
-			</a>
-		</div>
+		<CodeLinks />
 	{/if}
+	<noscript>
+		<CodeLinks />
+	</noscript>
 </div>
 
 <style>
@@ -242,24 +213,6 @@
 		height: 100%;
 		width: 100%;
 		overflow: hidden;
-	}
-	.links {
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		right: 0;
-		z-index: 1;
-		margin: 5px 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		font-size: 8vh;
-		font-size: 8svh;
-	}
-	.links a {
-		color: var(--text-color);
-		text-decoration: none;
-		margin: 0 5px;
 	}
 	.code .textbox {
 		top: 20%;
